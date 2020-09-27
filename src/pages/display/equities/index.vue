b9efd068cd397c3ef4d34eb5d43969edbcd263f1
xiaqinqing <1172313538@qq.com>
2020-09-27 20:10:18 +0800

    update

--- src/pages/display/equities/index.vue
+++ src/pages/display/equities/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="pages-display-equities">equities</div>
+  <div class="pages-display-equities">
+    <div>
+      <div>
+        <img class="juxing" src="../../../assets/images/juxing.png" alt="">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +16,12 @@ export default {
 
 <style lang="less" scoped>
 .pages-display-equities{
-  
+  .juxing {
+    width: 176px;
+    height: 86px;
+    background: linear-gradient(#71beff 0%, #284fb7 100%);
+    border-radius: 6px;
+  }
 }
+
 </style>
