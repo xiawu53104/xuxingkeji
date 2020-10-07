925628b76358809cdcd6651f5252b922b714c4c8
xiaqinqing <1172313538@qq.com>
2020-10-07 10:18:30 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,10 @@ const path = require('path')
 const resolve = url => path.resolve(__dirname, url)
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/dpzs' : '/',
+  configureWebpack: {
+    devtool: process.env.NODE_ENV === 'production' ? false : 'cheap-eval-source-map',
+  },
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
